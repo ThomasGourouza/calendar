b549432100c56f04c31a8ac7c5a54896e5102fe2
Thomas Gourouza <thomas.gourouza@de.markant.com>
2024-08-02 16:29:05 +0200

    update script

---
+++ data/lessons.js
@@ -0,0 +1,30 @@
+let lessons = [
+  {
+    calendarItemDate: new CalendarItemDate(9, 6, 2024),
+    quarterTimes: [6, 7, 8, 9],
+    room: "Room 2",
+    teacher: "Thomas",
+    level: "C1",
+  },
+  {
+    calendarItemDate: new CalendarItemDate(9, 6, 2024),
+    quarterTimes: [13, 14, 15, 16],
+    room: "Room 3",
+    teacher: "Thomas",
+    level: "C1",
+  },
+  {
+    calendarItemDate: new CalendarItemDate(10, 6, 2024),
+    quarterTimes: [3, 4],
+    room: "Room 1",
+    teacher: "Jean",
+    level: "B2",
+  },
+  {
+    calendarItemDate: new CalendarItemDate(9, 6, 2024),
+    quarterTimes: [18, 19, 20],
+    room: "Room 2",
+    teacher: "New",
+    level: "B2",
+  },
+];
--- script.js
+++ script.js
@@ -1,46 +1,14 @@
 const lang = "fr";
-const dayNames = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
 
 let calendarItems = [];
 
-// les quart d'heures de la journée de 8h00 à 20h00
+// les quarts d'heures de la journée de 8h00 à 20h00
 const allQuarterTimes = [];
 for (let i = 1; i <= 48; i++) {
   allQuarterTimes.push(new QuarterTime(i));
 }
 
 const roomObjects = rooms.map((name) => new RoomObject(name));
-
-let lessons = [
-  {
-    calendarItemDate: new CalendarItemDate(9, 6, 2024),
-    quarterTimes: [6, 7, 8, 9],
-    room: "Room 2",
-    teacher: "Thomas",
-    level: "C1",
-  },
-  {
-    calendarItemDate: new CalendarItemDate(9, 6, 2024),
-    quarterTimes: [13, 14, 15, 16],
-    room: "Room 3",
-    teacher: "Thomas",
-    level: "C1",
-  },
-  {
-    calendarItemDate: new CalendarItemDate(10, 6, 2024),
-    quarterTimes: [3, 4],
-    room: "Room 1",
-    teacher: "Jean",
-    level: "B2",
-  },
-  {
-    calendarItemDate: new CalendarItemDate(9, 6, 2024),
-    quarterTimes: [18, 19, 20],
-    room: "Room 2",
-    teacher: "New",
-    level: "B2",
-  },
-];
 
 fillSelectOptions("rooms", rooms);
 fillSelectOptions("levels", levels);
