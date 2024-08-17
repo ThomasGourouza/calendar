83838b8b302b7eb2820781a8008a521bdcf7599c
Thomas Gourouza <thomas.gourouza@de.markant.com>
2024-08-17 23:41:46 +0200

    update

--- utils/html-confirmations-builder-util.js
+++ utils/html-confirmations-builder-util.js
@@ -88,20 +88,20 @@ function buildHtmlLevelsConfirmations(levels) {
 
 function confirmHours(workingHours, numberDays, minHour) {
   const maxHours = numberDays * minHour;
-  if (workingHours.min === minHour && workingHours.max === maxHours) {
+  if (+workingHours.min === minHour && +workingHours.max === maxHours) {
     return "volume horaire indéterminé.";
   }
-  if (workingHours.min === workingHours.max) {
-    return `volume horaire d'exactement ${workingHours.min}h.`;
+  if (+workingHours.min === +workingHours.max) {
+    return `volume horaire d'exactement ${+workingHours.min}h.`;
   }
-  if (workingHours.min > minHour && workingHours.max === maxHours) {
-    return `volume horaire minimum de ${workingHours.min}h.`;
+  if (+workingHours.min > minHour && +workingHours.max === maxHours) {
+    return `volume horaire minimum de ${+workingHours.min}h.`;
   }
-  if (workingHours.min === minHour && workingHours.max < maxHours) {
-    return `volume horaire maximum de ${workingHours.max}h.`;
+  if (+workingHours.min === minHour && +workingHours.max < maxHours) {
+    return `volume horaire maximum de ${+workingHours.max}h.`;
   }
-  if (workingHours.min > minHour && workingHours.max < maxHours) {
-    return `volume horaire compris entre ${workingHours.min}h et ${workingHours.max}h.`;
+  if (+workingHours.min > minHour && +workingHours.max < maxHours) {
+    return `volume horaire compris entre ${+workingHours.min}h et ${+workingHours.max}h.`;
   }
   return "";
 }
